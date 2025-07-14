815450bcf2c632c3d0470d63168ca92134c6cca2
Azad Ayhan <azadayhan50@gmail.com>
2025-07-14 15:52:08 +0300

    ProductsPage added

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,7 @@
+export default function ProductList({products}) {
+    return(
+        <div>
+            {products.map((p) => p.title)}
+        </div>
+    )
+}
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import ProductList from "../components/ProductList";
+
 export default function ProductsPage() {
-    return(
-        <div className="">
-            <h1>Products Page</h1>
-        </div>
-    )
+  const [loadedProducts, setLoadedProducts] = useState([]);
+  const[loading,setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch("http://localhost:5001/products/");
+        const data = await response.json();
+        setLoadedProducts(data);
+      } catch(error) {
+        console.log(error)
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProducts();
+  }, []);
+
+  if(loading) return <h1>Loading...</h1>
+
+  return <ProductList products={loadedProducts}/>
 }
