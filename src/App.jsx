24153798eb56c356c056853a9adcf5b054a0cb24
Azad Ayhan <azadayhan50@gmail.com>
2025-07-23 02:33:56 +0300

    Axios Requests

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import RegisterPage from "./pages/Register";
 import ErrorPage from "./pages/errors/Error";
 import ServerErrorPage from "./pages/errors/ServerError";
 import NotFoundPage from "./pages/errors/NotFound";
+import { useEffect } from "react";
+import requests from "./api/apiClient";
 
 export const router = createBrowserRouter([
   {
@@ -27,17 +29,25 @@ export const router = createBrowserRouter([
       { path: "login", element: <LoginPage /> },
       { path: "register", element: <RegisterPage /> },
       { path: "cart", element: <CartPage /> },
-      { path: "errors", children: [
-        { index: true, element: <ErrorPage /> },
-        { path: "server-error", element: <ServerErrorPage /> },
-        { path: "not-found", element: <NotFoundPage /> },
-      ]},
-      {path: "*", element: <NotFoundPage/>}
+      {
+        path: "errors",
+        children: [
+          { index: true, element: <ErrorPage /> },
+          { path: "server-error", element: <ServerErrorPage /> },
+          { path: "not-found", element: <NotFoundPage /> },
+        ],
+      },
+      { path: "*", element: <NotFoundPage /> },
     ],
   },
 ]);
 
 function App() {
+  useEffect(() => {
+    requests.cart.get()
+      .then((cart) => console.log(cart))
+      .catch((error) => console.log(error));
+  }, []);
   return <RouterProvider router={router} />;
 }
 
