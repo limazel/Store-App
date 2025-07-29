4f9948360d26a7eacdaa10a0afc596640530ea9f
Azad Ayhan <azadayhan50@gmail.com>
2025-07-29 12:51:33 +0300

    Update Cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,6 @@
 import {
+  Button,
+  CircularProgress,
   IconButton,
   Paper,
   Table,
@@ -12,12 +14,33 @@ import {
 import { currencyTRY } from "../utils/formats";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useCartContext } from "../context/CartContext";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+import { useState } from "react";
+import requests from "../api/apiClient";
 
 export default function CartPage() {
   const { cart } = useCartContext();
+  const [loading, setLoading] = useState(false);
 
   if (!cart || cart.cartItems.length === 0)
     return <Typography component="h4">Sepetinizde ürün yok.</Typography>;
+
+  function handleAddItem(productId) {
+    setLoading(true);
+    requests.cart.addItem(productId)
+      .then((cart) => setCart(cart))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }
+
+  function handleRemoveItem(productId, quantity = 1) {
+     setLoading(true);
+    requests.cart.deleteItem(productId, quantity)
+      .then((cart) => setCart(cart))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }}>
@@ -26,7 +49,7 @@ export default function CartPage() {
             <TableCell sx={{ width: 100 }}></TableCell>
             <TableCell>Ürün</TableCell>
             <TableCell sx={{ width: 120 }}>Fiyat</TableCell>
-            <TableCell sx={{ width: 120 }}>Adet</TableCell>
+            <TableCell sx={{ width: 180 }}>Adet</TableCell>
             <TableCell sx={{ width: 120 }}>Toplam</TableCell>
             <TableCell sx={{ width: 50 }}></TableCell>
           </TableRow>
@@ -42,7 +65,23 @@ export default function CartPage() {
               </TableCell>
               <TableCell>{item.product.title}</TableCell>
               <TableCell>{currencyTRY.format(item.product.price)}</TableCell>
-              <TableCell>{item.product.quantity}</TableCell>
+              <TableCell>
+                <Button onClick={() => handleAddItem(item.product.productId)}>
+                  {loading ? (
+                    <CircularProgress size={20} />
+                  ) : (
+                    <AddCircleOutlineIcon />
+                  )}
+                </Button>
+                {item.product.quantity}
+                <Button onClick={() => handleRemoveItem(item.product.productId)}>
+                  {loading ? (
+                    <CircularProgress size={20} />
+                  ) : (
+                    <RemoveCircleOutlineIcon />
+                  )}
+                </Button>
+              </TableCell>
               <TableCell>
                 {currencyTRY.format(item.product.price * item.product.quantity)}
               </TableCell>
