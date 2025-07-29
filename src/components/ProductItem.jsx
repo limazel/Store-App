4993250a368416a89e1cc12ee2605d09b46ecfe9
Azad Ayhan <azadayhan50@gmail.com>
2025-07-29 15:24:46 +0300

    Product Details Product Update

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,17 +1,51 @@
-import { Grid, Paper, Typography } from "@mui/material";
+import { Button, CircularProgress, Grid, Paper, Stack, Typography } from "@mui/material";
+import { currencyTRY } from "../utils/formats";
+import ReportIcon from "@mui/icons-material/Report";
 
-export default function ProductItem({ product }) {
+export default function ProductItem({ product, handleAddItem, cartItem , isAdding }) {
   return (
     <Grid container spacing={2}>
-      <Grid size={{lg:4, md:5, sm:6, xs:12}}>
-        <Paper variant="outlined" sx={{p:3}}>
-            <img src={`http://localhost:5001/images/${product.image}`} style={{width: "100%"}}/>
+      <Grid size={{ lg: 4, md: 5, sm: 6, xs: 12 }}>
+        <Paper variant="outlined" sx={{ p: 3 }}>
+          <img
+            src={`http://localhost:5001/images/${product.image}`}
+            style={{ width: "100%" }}
+          />
         </Paper>
       </Grid>
-      <Grid size={{lg:8, md:7, sm:6, xs:12}}>
-        <Paper variant="outlined" sx={{p:3}}>
-            <Typography variant="h4" component="h1" color="secondary.dark">{product.title}</Typography>
-            <Typography variant="body1" >{product.description}</Typography>
+      <Grid size={{ lg: 8, md: 7, sm: 6, xs: 12 }}>
+        <Paper variant="outlined" sx={{ p: 3 }}>
+          <Typography variant="h4" component="h1" color="secondary.dark">
+            {product.title}
+          </Typography>
+          <Typography variant="body1">{product.description}</Typography>
+          <Typography variant="h5" color="secondary" sx={{ mt: 3 }}>
+            {currencyTRY.format(product.price)}
+          </Typography>
+          <Stack
+            direction="row"
+            display="flex"
+            alignItems="center"
+            sx={{ mt: 2, gap: 2, alignItems: "center" }}
+          >
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => handleAddItem(product.id)}
+            >
+              Sepete Ekle
+            </Button>
+            {cartItem?.product.quantity > 0 && (
+              <Typography variant="body2" display="flex" alignItems="center">
+                <ReportIcon color="secondary"/>
+                Sepette {cartItem.product.quantity} adet eklendi.
+              </Typography>
+            )}
+
+            {isAdding && (
+              <CircularProgress size={20}/>
+            )}
+          </Stack>
         </Paper>
       </Grid>
     </Grid>
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,11 +3,25 @@ import { useParams } from "react-router";
 import ProductItem from "../components/ProductItem";
 import Loading from "../components/Loading";
 import requests from "../api/apiClient";
+import { useCartContext } from "../context/CartContext";
 
 export default function ProductDetailsPage() {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
+  const [isAdding, setIsAdding] = useState(false);
   const [product, setProduct] = useState(null);
+  const {cart, setCart} = useCartContext();
+
+  const cartItem = cart?.cartItems.find(i => i.product.productId == product?.id);
+
+  function handleAddItem(productId) {
+    setIsAdding(true);
+    requests.cart.addItem(productId)
+      .then((cart) => setCart(cart))
+      .catch((error) => console.log(error))
+      .finally(() => setIsAdding(false));
+  }
+
   useEffect(
     () =>
       async function fetchProductDetails() {
@@ -29,7 +43,7 @@ export default function ProductDetailsPage() {
   
   return (
     <div className="">
-      <ProductItem product={product} />
+      <ProductItem product={product} handleAddItem={handleAddItem} cartItem={cartItem} isAdding={isAdding}/>
     </div>
   );
 }
