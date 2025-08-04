f025fad7e7ac8b8aa9ad472b4120bd5fd6691817
Azad Ayhan <azadayhan50@gmail.com>
2025-08-04 18:29:52 +0300

    Login Form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,43 @@
+import Avatar from "@mui/material/Avatar";
+import { Box, Button, Container, Paper, TextField, Typography } from "@mui/material";
+import LockOutlined from "@mui/icons-material/LockOutlined";
+
 export default function LoginPage() {
-    return(
-        <div className="">
-            <h1>LoginPage</h1>
-        </div>
-    )
+  return (
+    <Container maxWidth="xs">
+      <Paper sx={{ padding: 2 }} elevation={3}>
+        <Avatar sx={{ mx: "auto", mb: 2, color: "secondary.main" }}>
+          <LockOutlined />
+        </Avatar>
+        <Typography
+          component="h1"
+          variant="h5"
+          sx={{ textAlign: "center", mb: 2 }}
+        >
+          Login
+        </Typography>
+        <Box component="form" sx={{ mb: 2 }}>
+          <TextField
+            name="username"
+            label="Enter username"
+            size="small"
+            fullWidth
+            required
+            autoFocus
+            sx={{ mb: 2 }}
+          />
+          <TextField
+            name="password"
+            type="password"
+            label="Enter password"
+            size="small"
+            fullWidth
+            required
+            sx={{ mb: 2 }}
+          />
+          <Button type="submit" variant="contained" fullWidth color="secondary">Submit</Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,51 @@
+import Avatar from "@mui/material/Avatar";
+import { Box, Button, Container, Paper, TextField, Typography } from "@mui/material";
+import LockOutlined from "@mui/icons-material/LockOutlined";
+
 export default function RegisterPage() {
-    return(
-        <div className="">
-            <h1>RegisterPage</h1>
-        </div>
-    )
+  return (
+    <Container maxWidth="xs">
+      <Paper sx={{ padding: 2 }} elevation={3}>
+        <Avatar sx={{ mx: "auto", mb: 2, color: "secondary.main" }}>
+          <LockOutlined />
+        </Avatar>
+        <Typography
+          component="h1"
+          variant="h5"
+          sx={{ textAlign: "center", mb: 2 }}
+        >
+          Register
+        </Typography>
+        <Box component="form" sx={{ mb: 2 }}>
+          <TextField
+            name="username"
+            label="Enter username"
+            size="small"
+            fullWidth
+            required
+            autoFocus
+            sx={{ mb: 2 }}
+          />
+          <TextField
+            name="email"
+            label="Enter email"
+            size="small"
+            fullWidth
+            required
+            sx={{ mb: 2 }}
+          />
+          <TextField
+            name="password"
+            type="password"
+            label="Enter password"
+            size="small"
+            fullWidth
+            required
+            sx={{ mb: 2 }}
+          />
+          <Button type="submit" variant="contained" fullWidth color="secondary">Submit</Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
 }
