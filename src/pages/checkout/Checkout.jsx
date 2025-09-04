c00f38fa79bca66b5977f908d688ae73352ad24b
Azad Ayhan <azadayhan50@gmail.com>
2025-09-04 09:41:11 +0300

    CheckoutPage

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,3 +1,22 @@
+import { Paper, Grid } from "@mui/material";
+import Info from "./Info"
+import AddressForm from "./AddressForm"
+import PaymentForm from "./PaymentForm"
+import Review from "./Review"
+
 export default function CheckoutPage() {
-    return <h1>Checkout</h1>
+  return (
+    <Paper>
+      <Grid container spacing={3}>
+        <Grid size={4} sx={{p:3, borderRight: "1px solid ", borderColor: "divider"}}>
+          <Info />
+        </Grid>
+        <Grid size={8} sx={{p:3}}>
+          <AddressForm />
+          <PaymentForm/>
+          <Review/>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 }
